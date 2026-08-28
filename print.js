61e0cbe7e2fa5ce774a0aea9e23bc1954a320d06
kikuchimkt-afk <kikuchi.mkt@gmail.com>
2026-08-28 19:26:38 +0900

    feat: add 2024 grade 4 exam PDFs

--- print.js
+++ print.js
@@ -200,6 +200,24 @@
             downloadName: 'ReadPass_EIKEN_Grade5_2025-3_Practice_Exam_Large_Type_v1.pdf',
             pages: 6
         },
+        'grade4/2024-1': {
+            label: '英検4級 2024年度 第1回',
+            path: 'output/pdf/ReadPass_EIKEN_Grade4_2024-1_Practice_Exam_Large_Type_v1.pdf?v=20260828-eiken-grade4-2024-v1',
+            downloadName: 'ReadPass_EIKEN_Grade4_2024-1_Practice_Exam_Large_Type_v1.pdf',
+            pages: 11
+        },
+        'grade4/2024-2': {
+            label: '英検4級 2024年度 第2回',
+            path: 'output/pdf/ReadPass_EIKEN_Grade4_2024-2_Practice_Exam_Large_Type_v1.pdf?v=20260828-eiken-grade4-2024-v1',
+            downloadName: 'ReadPass_EIKEN_Grade4_2024-2_Practice_Exam_Large_Type_v1.pdf',
+            pages: 11
+        },
+        'grade4/2024-3': {
+            label: '英検4級 2024年度 第3回',
+            path: 'output/pdf/ReadPass_EIKEN_Grade4_2024-3_Practice_Exam_Large_Type_v1.pdf?v=20260828-eiken-grade4-2024-v1',
+            downloadName: 'ReadPass_EIKEN_Grade4_2024-3_Practice_Exam_Large_Type_v1.pdf',
+            pages: 11
+        },
         'grade4/2025-1': {
             label: '英検4級 2025年度 第1回',
             path: 'output/pdf/ReadPass_EIKEN_Grade4_2025-1_Practice_Exam_Large_Type_v1.pdf?v=20260828-eiken-grade4-v1',
